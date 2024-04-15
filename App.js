dc6329390b65a172dcdfc0e657d89563e4b1c396
chenborui1 <150975091+chenborui1@users.noreply.github.com>
2024-04-15 16:02:30 -0400

    implemented userse

--- App.js
+++ App.js
@@ -1,15 +1,50 @@
+import "dotenv/config";
 import express from "express";
 import Lab5 from "./Lab5.js";
 import cors from "cors";
 import Hello from "./Hello.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
+import mongoose from "mongoose";
+import UserRoutes from "./Kanbas/Users/routes.js";
+import session from "express-session";
 
 const app = express()
-app.use(cors())
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+mongoose.connect(CONNECTION_STRING);
+
+
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+    
+}));
+
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+      domain: process.env.HTTP_SERVER_DOMAIN,
+    };
+  }
+  
+app.use(session(sessionOptions));
+
+
+
 app.use(express.json())
+
+
+
 Hello(app)
 CourseRoutes(app)
 ModuleRoutes(app)
 Lab5(app)
+UserRoutes(app);
 app.listen(process.env.PORT || 4000);
